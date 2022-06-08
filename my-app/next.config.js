47356b1859200526ac3b3f628dd39fe3cbf4e373
zaidanammar <zaidanammar23@gmail.com>
2022-06-08 13:36:42 +0700

    :sparkles: handle redirect

--- my-app/next.config.js
+++ my-app/next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [{
+      source: '/',
+      destination: '/question-one',
+      permanent: true,
+    }, ]
+  },
 }
 
 module.exports = nextConfig
